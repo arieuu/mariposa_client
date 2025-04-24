eeef89696027561b51134b3d19a55404b499eb00
Pliskin <arieljanickcarvalho@gmail.com>
2025-04-24 18:04:48 +0100

    removed chakra ui completely

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Provider } from './components/ui/provider.tsx';
-import { ColorModeProvider, DarkMode } from './components/ui/color-mode.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
